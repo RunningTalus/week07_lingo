194032608398fe97bc41d551a43b25589bf45856
runningtalus <dave@davemay.org>
2014-05-18 14:57:19 -0600

    Made changes to main.js messing around with jQuery

--- public/js/main.js
+++ public/js/main.js
@@ -31,7 +31,9 @@ $(document).on('ready', function(){
 			
 			$.post('/quiz/question', languages, function(question, err){
 				$('#container').empty();
-				$('#container').addClass('quiz-template');
+				// $('#container').addClass('quiz-template');
+				$(document).addClass('quiz-template');
+				$('#container').after(<div>"quiz-template"</div>);
 				
 				console.log(question);
 
@@ -40,6 +42,11 @@ $(document).on('ready', function(){
 
 				var questionAnswer = "What is the translation for this word in " + langTo + " ?";	
 				console.log(questionAnswer);
+
+				$('.quiz-template').after('form-group');
+				$('.form-group').addClass('qa-slot');
+				$('#qa-slot').addClass('q-holder', 'a-holder');
+
 
 				//need to continue working on jQuery functionality for form and quiz
 			});
@@ -50,3 +57,16 @@ $(document).on('ready', function(){
 
 	});
 });	
+
+// <div class="form-group has-success">
+//   <label class="control-label" for="inputSuccess1">Input with success</label>
+//   <input type="text" class="form-control" id="inputSuccess1">
+// </div>
+// <div class="form-group has-warning">
+//   <label class="control-label" for="inputWarning1">Input with warning</label>
+//   <input type="text" class="form-control" id="inputWarning1">
+// </div>
+// <div class="form-group has-error">
+//   <label class="control-label" for="inputError1">Input with error</label>
+//   <input type="text" class="form-control" id="inputError1">
+// </div>
